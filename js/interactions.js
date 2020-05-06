f486b801ac5096088e10d91507f16a89a88ee39d
Olivia Raymond <olivia.raymond97@gmail.com>
2020-05-06 09:10:16 -0600

    format

--- js/interactions.js
+++ js/interactions.js
@@ -1,8 +1,8 @@
 
 var randomNum = 0,
-	guessNum = 0;
+		guessNum = 0;
 
-//changes words on screen; not on HTML
+// updating the UI with new message
 function writeMessage(elementId, message, appendMessage) {
 	var elemToUpdate = document.getElementById(elementId);
 	if (appendMessage) {
@@ -12,14 +12,7 @@ function writeMessage(elementId, message, appendMessage) {
 	}
 };
 
-//new game, which includes a new random number and guessNum set to 0
-function newGame() {
-	randomNum = Math.floor(Math.random() * 100) + 1;
-	guessNum = 0;
-	writeMessage('guessList', '');
-}
-
-//method called to check if range of player input is allowed
+// method called to check if range of player input is allowed
 function guessRange(guess) {
 	return (guess > 0 && guess < 101);
 }
@@ -29,14 +22,13 @@ function playerGuessed() {
 	var instructions = document.getElementById('instructions');
 	var guessList = document.getElementById('guessList');
 	if (playerGuessed.length == 0 || ! guessRange(playerGuessed)) {
-		//when player enters nothing or a number that isn't in the range
+		// when player enters nothing or a number that isn't in the range
 		writeMessage('instructions', '<p>No, silly! Enter a number between 1-100 and press the Guess button!</p>');
 	} else if (playerGuessed.indexOf('.') != -1) {
 		// when player enters a number that is not a whole number
 		writeMessage('instructions', '<p>Try again! Whole numbers only. Enter a whole number between 1-100 and press the Guess button!</p>');
 	} else {
 		guessNum++;
-
 		if (playerGuessed == randomNum) {
 			// the player got the number correct
 			writeMessage('instructions', '<p><img src="img/youwin.jpg"><br>YOU GOT IT! It took you ' + guessNum +' guesses to get ' + randomNum + '. Play again! </p>');
@@ -51,17 +43,22 @@ function playerGuessed() {
 			writeMessage('guessList', '<li>' + playerGuessed + ' ...too high</li>', true);
 		}
 		if (guessNum > 5) {
-			//when player takes longer than 5 guesses
+			// when player takes longer than 5 guesses
 			writeMessage('instructions', '<p>Are you ever going to get this? You alreay have guessed '+ guessNum + ' times! Keep trying...</p>');
-		} else{
-
 		}
 	}
-
-//gets player's input
+	// gets player's input
 	document.getElementById('playerGuess').value = '';
 }
 
+// new game, which includes a new random number and guessNum set to 0
+function newGame() {
+	randomNum = Math.floor(Math.random() * 100) + 1;
+	guessNum = 0;
+	writeMessage('guessList', '');
+}
+
+// register
 window.onload = function() {
 	newGame();
 	document.getElementById('button').addEventListener('click', playerGuessed);
